4c24e61088a7f776186788ea55ee658a04c6633b
Omar Lizardo <omarlizardo@gmail.com>
2026-08-18 10:43:48 -0700

    boof

--- resume_master_queue.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Convenient wrapper to reliably start the master queue in the background
-nohup ./run_master_queue_smart.sh < /dev/null > logs/run_master_queue_all.log 2>&1 &
-echo "Master queue started in the background. Tail logs/run_master_queue_all.log to monitor."
